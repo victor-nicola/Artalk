ede72742a78daa5255bcc2d6c5133155df62a5fc
PalamidaUsturoi <99142809+PalamidaUsturoi@users.noreply.github.com>
2023-02-24 09:41:07 +0200

    test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ArtalkLinux
-# ArtalkLinux
+# Artalk
+O retea de socializare care vine in ajutorul artistilor ce nu se mai pot promova eficient online din cauza faptului ca toate retelele de socializare incurajeaza doar continutul video, astfel ilustratori, pictori, sculptori, fotografi etc au ramas fara o platforma care sa-i valorizeze.
--- front-end/artalk/babel.config.js
+++ front-end/artalk/babel.config.js
@@ -3,13 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        "module-resolver",
-        {
-          extensions: [".tsx", ".ts", ".js", ".json"],
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+      ['react-native-reanimated/plugin', {relativeSourceLocation: true}]
+    ]
   };
 };
